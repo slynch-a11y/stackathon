738e8db801c9531696b8d3645f6c13de4441bd2f
Sarah Lynch <sarah@alum.rpi.edu>
2018-09-09 20:28:21 -0400

    deploy

--- client/src/components/Bunny.js
+++ client/src/components/Bunny.js
@@ -3,7 +3,6 @@ import React from 'react'
 
 class Bunny extends React.Component {
 
-
   render(){
     return (
       <div><img src='https://chore-bunny.herokuapp.com/chorebunny.png' /></div>
@@ -12,7 +11,6 @@ class Bunny extends React.Component {
 
 
 }
-
 
 export default Bunny
 
